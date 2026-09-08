4f840543d72610bf74b20337d84db89b121c5848
etospalu-jpg <etospalu@gmail.com>
2026-09-08 12:55:35 +0800

    refactor: dispatch journal via existing secure function

--- api/secure.js
+++ api/secure.js
@@ -1,1 +1,7 @@
-module.exports=require('../secure-cookie');
+const secure=require('../secure-cookie');
+const mentoringJournal=require('../mentoring-journal-handler');
+module.exports=async function handler(req,res){
+  const fn=String(req.body?.function||'');
+  if(fn==='getMentoringJournal'||fn==='saveMentoringJournal')return mentoringJournal(req,res);
+  return secure(req,res);
+};
